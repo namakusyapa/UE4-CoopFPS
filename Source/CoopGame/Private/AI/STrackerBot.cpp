1bf6fd7bb2fb6315e2df179fd64ade4f47b02091
root <root@a3.localdomain>
2020-03-08 19:15:13 +0900

    update codes

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -9,6 +9,10 @@
 #include "GameFramework/Character.h"
 #include "DrawDebugHelpers.h"
 #include "../../Public/Components/SHealthComponent.h"
+#include "..\..\Public\AI\STrackerBot.h"
+#include "SCharacter.h"
+#include "Components/SphereComponent.h"
+#include "Sound/SoundCue.h"
 
 // Sets default values
 ASTrackerBot::ASTrackerBot()
@@ -24,18 +28,32 @@ ASTrackerBot::ASTrackerBot()
 	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
 	HealthComp->OnHealthChanged.AddDynamic(this, &ASTrackerBot::HandleTakeDamage);
 
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetSphereRadius(200);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SphereComp->SetupAttachment(RootComponent);
+
 	bUseVelocityChange = false;
 
 	MovementForce = 1000;
 	RequireDistanceToTarget = 100;
+
+	ExplosionRadius = 200;
+	ExplosionDamage = 40;
+	SelfDamageInterval = 0.25f;
+
+	bExploded = false;
 }
 
 // Called when the game starts or when spawned
 void ASTrackerBot::BeginPlay()
 {
 	Super::BeginPlay();
-
-	NextPathPoint = GetNextPathPoint();
+	if (Role == ROLE_Authority) {
+		NextPathPoint = GetNextPathPoint();
+	}
 }
 
 void ASTrackerBot::HandleTakeDamage(USHealthComponent* OwningHealthComp, float Health, float HealthDelta,
@@ -43,7 +61,20 @@ void ASTrackerBot::HandleTakeDamage(USHealthComponent* OwningHealthComp, float H
 {
 	// Explode on hitpoints == 0
 
-	UE_LOG(LogTemp, Log, TEXT("Health  %s of %s"), *FString::SanitizeFloat(Health), *GetName()) ;
+
+	if (MatInst == nullptr) {
+		MatInst = MeshComp->CreateAndSetMaterialInstanceDynamicFromMaterial(0, MeshComp->GetMaterial(0));
+	}
+
+	if (MatInst) {
+		MatInst->SetScalarParameterValue("LastTimeDamageTakenIn", GetWorld()->TimeSeconds);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Health changed  %s of %s"), *FString::SanitizeFloat(Health), *GetName());
+
+	if (Health <= 0) {
+		SelfDestruct();
+	}
 }
 
 FVector ASTrackerBot::GetNextPathPoint() {
@@ -60,27 +91,80 @@ FVector ASTrackerBot::GetNextPathPoint() {
 	return GetActorLocation();
 }
 
+void ASTrackerBot::SelfDestruct()
+{
+	if (bExploded) {
+		return;
+	}
+
+	bExploded = true;
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+
+	MeshComp->SetVisibility(false, true);
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	if (Role == ROLE_Authority) {
+
+		TArray<AActor*> IgnoredActors;
+		IgnoredActors.Add(this);
+
+		UGameplayStatics::ApplyRadialDamage(this, ExplosionDamage, GetActorLocation(), ExplosionRadius, nullptr, IgnoredActors, this, GetInstigatorController(), true);
+
+		DrawDebugSphere(GetWorld(), GetActorLocation(), ExplosionRadius, 12, FColor::Red, false, 2.0f, 0, 1.0f);
+
+		UGameplayStatics::PlaySoundAtLocation(this, ExplodeSound, GetActorLocation());
+		
+		SetLifeSpan(2.0f);
+	}
+}
+
 // Called every frame
 void ASTrackerBot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
-
-	if (DistanceToTarget <= RequireDistanceToTarget)
+	if (Role == ROLE_Authority && !bExploded)
 	{
-		NextPathPoint = GetNextPathPoint();
-		//DrawDebugString(GetWorld(), GetActorLocation(), "Reached");
+		float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
+
+		if (DistanceToTarget <= RequireDistanceToTarget)
+		{
+			NextPathPoint = GetNextPathPoint();
+			//DrawDebugString(GetWorld(), GetActorLocation(), "Reached");
+		}
+		else {
+			FVector ForceDirection = NextPathPoint - GetActorLocation();
+			ForceDirection.Normalize();
+
+			ForceDirection *= MovementForce;
+
+			MeshComp->AddForce(ForceDirection, NAME_None, bUseVelocityChange);
+			DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), GetActorLocation() + ForceDirection, 32, FColor::Yellow, false, 0.0f, 0, 1.0f);
+		}
+
+		DrawDebugSphere(GetWorld(), NextPathPoint, 20, 12, FColor::Yellow, false, 4.0f, 1.0f);
 	}
-	else {
-		FVector ForceDirection = NextPathPoint - GetActorLocation();
-		ForceDirection.Normalize();
+}
 
-		ForceDirection *= MovementForce;
+void ASTrackerBot::DamageSelf() {
+	UGameplayStatics::ApplyDamage(this, 20, GetInstigatorController(), this, nullptr);
+}
 
-		MeshComp->AddForce(ForceDirection, NAME_None, bUseVelocityChange);
-		DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), GetActorLocation() + ForceDirection, 32, FColor::Yellow, false, 0.0f, 0, 1.0f);
+void ASTrackerBot::NotifyActorBeginOverlap(AActor* OtherActor) {
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if(!bStartedSelfDestruction && !bExploded){
+		ASCharacter* PlayerPawn = Cast<ASCharacter>(OtherActor);
+		if (PlayerPawn) {
+
+			//Overlap with player
+			if (Role == ROLE_Authority) {
+				//start self destruct
+				GetWorldTimerManager().SetTimer(TimerHandle_SelfDamage, this, &ASTrackerBot::DamageSelf, SelfDamageInterval, true, 0.0f);
+			}
+			bStartedSelfDestruction = true;
+
+			UGameplayStatics::SpawnSoundAttached(SelfDestructSound, RootComponent);
+		}
 	}
-
-	DrawDebugSphere(GetWorld(), NextPathPoint, 20, 12, FColor::Yellow, false, 4.0f, 1.0f);
 }
---
+++ Source/CoopGame/Private/SPickupActor.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPickupActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/DecalComponent.h"
+#include "SPowerupActor.h"
+#include "TimerManager.h"
+
+// Sets default values
+ASPickupActor::ASPickupActor()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetSphereRadius(75.0f);
+	RootComponent = SphereComp;
+
+	DecalComp = CreateDefaultSubobject <UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->SetRelativeRotation(FRotator(90, 0.0f, 0.0f));
+	DecalComp->DecalSize = FVector(64, 75, 75);
+	DecalComp->SetupAttachment(RootComponent);
+
+
+}
+
+// Called when the game starts or when spawned
+void ASPickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Respawn();
+}
+
+void ASPickupActor::Respawn()
+{
+	if(PowerupClass == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PowerUp Class is nullptr in %s. Please update your Blueprint"));
+		return;
+	}
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	PowerupInstance = GetWorld()->SpawnActor<ASPowerupActor>(PowerupClass, GetTransform(), SpawnParams);
+}
+
+void ASPickupActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (PowerupInstance)
+	{
+		PowerupInstance->ActivatePowerup();
+		PowerupInstance = nullptr;
+
+		//Set timer to respawn powerup
+		GetWorldTimerManager().SetTimer(TimerHandle_RespawnTimer, this, &ASPickupActor::Respawn, CooldownDuration);
+
+	}
+
+}
